6caa1787a4664856c8c45434a4728353ec1981bf
ManuDev03 <manuprajan03@gmail.com>
2021-08-06 20:52:11 +0530

    using interceptors manipulating request body

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,6 +4,10 @@ export class AuthInterceptorService implements HttpInterceptor{
     intercept(req:HttpRequest<any>,next:HttpHandler){
         // if(req.body) conditions 
         console.log("Request is on the way")
-        return next.handle(req)
+        console.log(req.url)
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth','xyz')
+        })
+        return next.handle(modifiedRequest)
     }
 }
