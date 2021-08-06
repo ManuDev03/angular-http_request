8dcdabbb4c91755dacb35d0f8766a87ba336e379
ManuDev03 <manuprajan03@gmail.com>
2021-08-06 17:43:42 +0530

    using subjects for error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import {map} from 'rxjs/operators'
 import {Post} from './post.model';
 import {PostService} from './posts.service'
+import { Subscription } from 'rxjs';
 
 @Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     title = 'http-request';
     loadedPosts : Post[] = [];
     isFetching = false;
     error = null
+    private errorSub: Subscription
 
     constructor(private http : HttpClient, private postService : PostService) {}
 
 
     ngOnInit() {
+        this.errorSub = this.postService.error.subscribe(errorMessage => {
+            this.error = errorMessage
+        })
         this.isFetching = true;
         this.postService.fetchPosts().subscribe(posts => {
             this.isFetching = false;
@@ -42,5 +47,9 @@ export class AppComponent implements OnInit {
         this.postService.deletePosts().subscribe(()=> {
             this.loadedPosts = []
         })
+    }
+
+    ngOnDestroy() {
+        this.errorSub.unsubscribe() 
     }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,9 +2,11 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from '@angular/common/http'
 import {Post} from "./post.model";
 import {map} from "rxjs/operators"
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostService {
+    error = new Subject<string>()
     constructor(private http : HttpClient) {}
     createAndStorePost(title : string, content : string) {
         const postData: Post = {
@@ -13,6 +15,8 @@ export class PostService {
         }
         this.http.post<{name:string}>('https://ng-complete-guide-e0be1-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
             console.log(responseData);
+        },error => {
+            this.error.next(error.message)
         });
 
     }
